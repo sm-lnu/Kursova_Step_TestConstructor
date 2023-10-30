32bd54aaf928d297565fee7aaa7f3ed2a6dbadb0
sm-lnu <mykhailo.stefanyshyn@lnu.edu.ua>
2023-10-30 18:40:22 +0200

    Add project files.

---
+++ Test Constructor/Additional Classes/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Constructor.Additional_Classes
+{
+    class Test
+    {
+        public String textOfQuestion { get; set; }
+        public long points { get; set; }
+        public int countOfAnswers { get; set; }
+        public List<Question> questions { get; set; }
+
+        public Test(String textOfQuetion, long points, int countOfAnswers)
+        {
+            this.textOfQuestion = textOfQuetion;
+            this.points = points;
+            this.countOfAnswers = countOfAnswers;
+        }
+    }
+}
